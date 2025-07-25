cc4f98bb707c1f9d295af88905571ee0ca42e515
subaladhikari <subaladhikari01@gmail.com>
2025-07-25 18:54:00 -0500

    4th Kyu: reverseQuickly.js

--- 4th Kyu/reverseQuickly.js
+++ 4th Kyu/reverseQuickly.js
@@ -1,3 +1,5 @@
+// 4kyu - Wild Reverse
+
 /*
 Typically, reversing an array is a pretty straightforward task even for novice programmers. But not when a crowd of angry zombies scratching your door, looking for a fresh brains. In this case even skilled ninja-geek probably prefer to quickly push his code on github to have enough time to find a chainsaw. So there's two obstacles:
 
