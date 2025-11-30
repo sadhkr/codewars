90fff28a78e586b6c41a139100c5d7d921d9c5c4
subaladhikari <subaladhikari01@gmail.com>
2025-11-30 18:58:00 -0500

    6th Kyu: countingDuplicates.js

--- 6th Kyu/countingDuplicates.js
+++ 6th Kyu/countingDuplicates.js
@@ -1,41 +1,16 @@
-/*
-Count the number of Duplicates
-Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string.
-The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+// 6 kyu - Counting Duplicates
 
-Example
-"abcde" -> 0 # no characters repeats more than once
-"aabbcde" -> 2 # 'a' and 'b'
-"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
-"indivisibility" -> 1 # 'i' occurs six times
-"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
-"aA11" -> 2 # 'a' and '1'
-"ABBA" -> 2 # 'A' and 'B' each occur twice
-*/
+// Description:
 
-//Answer//
+// Count the number of Duplicates
+// Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphanumeric characters, including digits, uppercase and lowercase alphabets.
 
-function duplicateCount(text){
-  return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
-}
+// Example
+// "abcde" -> 0 # no characters repeats more than once
+// "aabbcde" -> 2 # 'a' and 'b'
+// "aabbcdeB" -> 2 # 'a' and 'b'
+// "indivisibility" -> 1 # 'i'
+// "Indivisibilities" -> 2 # 'i' and 's'
+// "aa11" -> 2 # 'a' and '1'
 
-//OR//
-
-function duplicateCount(text){
-//B turns string into array of lowercase letters
-let B = text.toLowerCase().split('')
-
-//C makes an array with the unique elements from B
-let C = [...new Set (B)]
-
-// count filters the array by the given character
-let count = x=> {
- return B.filter(y=>x===y).length
-}
-
-//C maps the count of the characters in text
-let D = C.map(count)
-
-//returns the count of characters greater than 1
-return D.filter(x=>x>1).length
-}
+let duplicateCount = str => text.toLowerCase().split('').sort().join('').match(/([^])\1+/ || []).length;
