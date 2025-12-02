0edf8def62837fdd490e085e0ae955d0f661ee76
subaladhikari <subaladhikari01@gmail.com>
2025-12-02 18:14:00 -0500

    6th Kyu: duplicateEncoder.js

--- 6th Kyu/duplicateEncoder.js
+++ 6th Kyu/duplicateEncoder.js
@@ -1,39 +1,21 @@
-/*
-The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string.
-Ignore capitalization when determining if a character is a duplicate.
+// 6kyu - Duplicate Encoder
 
-Examples
-"din"      =>  "((("
-"recede"   =>  "()()()"
-"Success"  =>  ")())())"
-"(( @"     =>  "))((" 
-Notes
+// The goal of this exercise is to convert a string to a new string where each 
+// character in the new string is '(' if that character appears only once in the 
+// original string, or ')' if that character appears more than once in the original 
+// string. Ignore capitalization when determining if a character is a duplicate.
 
-Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX", the "XXX" is the expected result, not the input!
-*/
+// Examples:
 
-//Answer//
+// "din" => "((("
 
-function duplicateEncode(word){
-//B turns string into array of lowercase letters
-let B = word.toLowerCase().split('')
+// "recede" => "()()()"
 
-// count filters the array by the given character
-let count = x=> {
- return B.filter(y=>x===y).length
-}
+// "Success" => ")())())"
 
-//C maps the count of the characters to the index of the characters
-let C = B.map(count)
+// "(( @" => "))(("
 
-//Brackets is a function that returns parantheses for our parameters
-let Brackets = (x) => {
-return x>1? ')': '('
-};
-
-//D maps the Brackets function to our array
-let D = C.map(Brackets)
-
-//Our output needs to be a string, so we join our array}
-return D.join('')
+let duplicateEncode = str => {
+  str = str.toLowerCase().split("");
+  return str.map( a => (str.indexOf(a) === str.lastIndexOf(a)) ? "(" : ")").join("");
 }
