f1925f8a062c4344629f1770cc8a8fe4ea465198
subaladhikari <subaladhikari01@gmail.com>
2025-12-04 18:15:00 -0500

    6th Kyu: multiplicationTable.js

--- 6th Kyu/multiplicationTable.js
+++ 6th Kyu/multiplicationTable.js
@@ -1,18 +1,18 @@
-/*
-Your task, is to create NxN multiplication table, of size provided in parameter.
+// 6kyu - Multiplication Table 
 
-for example, when given size is 3:
+// Your task, is to create NxN multiplication table, of size provided in parameter.
 
-1 2 3
-2 4 6
-3 6 9
-for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
-*/
+// for example, when given size is 3:
 
-//Answer//
+// 1 2 3
+// 2 4 6
+// 3 6 9
+// for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]
 
 multiplicationTable = function(size) {
-let A = [...new Array(size).keys()].map(x=>x+1)
-return A.map(x=>A.map(y=>y*x))
+  return Array.apply(null, new Array(size)).map(function(val, i) {
+    return Array.apply(null, new Array(size)).map(function(val, j) {
+      return (i+1) * (j+1);
+    });
+  });
 }
- 
