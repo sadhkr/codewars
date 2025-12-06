8270b75586ad2d8a7678443bc329dde740d0edee
subaladhikari <subaladhikari01@gmail.com>
2025-12-06 18:43:00 -0500

    6th Kyu: pairOfGloves.js

--- 6th Kyu/pairOfGloves.js
+++ 6th Kyu/pairOfGloves.js
@@ -1,26 +1,25 @@
+// 6kyu - Pair of Gloves
 
-/*
-Winter is coming, you must prepare your ski holidays. The objective of this kata is to determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
+// Pair of gloves
+// Winter is comming, you must prepare your ski holidays. The objective of this kata is to determine the number of pair of gloves you can constitute from the gloves you have in your drawer.
 
-A pair of gloves is constituted of two gloves of the same color.
+// A pair of gloves is constituted of two gloves of the same color.
 
-You are given an array containing the color of each glove.
+// You are given an array containing the color of each glove.
 
-You must return the number of pair you can constitute.
+// You must return the number of pair you can constitute.
 
-You must not change the input array.
+// You must not change the input array.
 
-Example :
+// Example :
 
-let myGloves = ['red','green','red','blue','blue'];
-numberOfPairs(myGloves) == 2;// red and blue
+// let myGloves = ['red','green','red','blue','blue'];
+// numberOfPairs(myGloves) == 2;// red and blue
 
-let redGloves = ['red','red','red','red','red','red'];
-numberOfPairs(redGloves) == 3; // 3 red pairs
-*/
+// let redGloves = ['red','red','red','red','red','red'];
+// numberOfPairs(redGloves) == 3; // 3 red pairs
 
-//Answer//
-function numberOfPairs(gloves)
-{
-  return [...new Set(gloves)].map(x=>Math.floor(gloves.filter(y=>y===x).length/2)).reduce((a,b)=>a+b,0)
+function numberOfPairs(gloves) {
+  let arr = Array.from(new Set(gloves));
+  return arr.reduce((a,b) => a + Math.floor(gloves.join('').match(new RegExp(b, 'g')).length / 2), 0);
 }
