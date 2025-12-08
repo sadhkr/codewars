fb05a3e9f6a36cd39c586d32ed9edc288e33bd4a
subaladhikari <subaladhikari01@gmail.com>
2025-12-08 18:01:00 -0500

    6th Kyu: splitStrings.js

--- 6th Kyu/splitStrings.js
+++ 6th Kyu/splitStrings.js
@@ -1,25 +1,17 @@
-/*
-Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+// 6 kyu - Split Strings
 
-Examples:
+// Description:
 
-solution('abc') // should return ['ab', 'c_']
-solution('abcdef') // should return ['ab', 'cd', 'ef']
-*/
+// Complete the solution so that it splits the string into pairs of two characters. 
+// If the string contains an odd number of characters then it should replace the 
+// missing second character of the final pair with an underscore ('_').
 
-//Answer//
+// Examples:
 
-function solution(str){
-   return (str + "_").match(/../g)||[];
-}
-
-//Or//
+// solution('abc') // should return ['ab', 'c_']
+// solution('abcdef') // should return ['ab', 'cd', 'ef']
 
 function solution(str){
-if(str.length%2!==0){str+='_'}
-let S = []
-for(let i = 0; i<str.length ; i+=2){
-S.push(str[i]+str[i+1])
-}
-return S
+   str = (str.length % 2 === 0) ? str : str + "_";
+   return str.match(/.{1,2}/g);
 }
