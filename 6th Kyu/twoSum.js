09c28b5a1c58644b2699e74965b837d8a734d377
subaladhikari <subaladhikari01@gmail.com>
2025-12-10 18:27:00 -0500

    6th Kyu: twoSum.js

--- 6th Kyu/twoSum.js
+++ 6th Kyu/twoSum.js
@@ -1,19 +1,21 @@
-/*
-Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple like so: (index1, index2).
+// 6kyu - Two Sum
 
-For the purposes of this kata, some tests may have multiple answers; any valid solutions will be accepted.
+// Write a function that takes an array of numbers (integers for the tests) and 
+// a target number. It should find two different items in the array that, when 
+// added together, give the target value. The indices of these items should then 
+// be returned in an array like so: [index1, index2].
 
-The input will always be valid (numbers will be an array of length 2 or greater, and all of the items will be numbers; target will always be the sum of two different items from that array).
+// For the purposes of this kata, some tests may have multiple answers; any valid 
+// solutions will be accepted.
 
-Based on: http://oj.leetcode.com/problems/two-sum/
+// The input will always be valid (numbers will be an array of length 2 or 
+// 	greater, and all of the items will be numbers; target will always be the 
+// 	sum of two different items from that array).
 
-twoSum [1, 2, 3] 4 === (0, 2)
-*/
-
-//Answer//
+// Based on: http://oj.leetcode.com/problems/two-sum/
 
 function twoSum(n, t) {
-for(let i = 0; i<n.length;i++){
-if (n.indexOf(t-n[i])!==-1){return [i, n.lastIndexOf(t-n[i])]}
-}
+    for (let i = 0; i < n.length-1; i++)
+        for (let j = i+1; j < n.length; j++)
+            if (n[i] + n[j] === t) return [i, j];
 }
